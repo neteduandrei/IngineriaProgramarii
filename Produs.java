5629d67164083163c73a287bc456d89b9a607c8d
neteduandrei <andrei.netedu2009@gmail.com>
2017-03-22 14:57:50 +0200

    Add files via upload
    Update2

--- Produs.java
+++ Produs.java
@@ -2,4 +2,7 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("195fb307-6663-4db9-a06b-3b4b18b8158d")
 public interface Produs {
+    @objid ("eadaaeda-007d-41e6-839e-be228342f7f0")
+    void getPrice();
+
 }
