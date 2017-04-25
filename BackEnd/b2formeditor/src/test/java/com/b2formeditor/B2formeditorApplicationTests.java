d00ed358cd4f3d524b7815d0dce4784be78d1595
Andrei Netedu <andrei.netedu2009@gmail.com>
2017-04-25 15:41:32 +0300

    added io.spring controller

---
+++ BackEnd/b2formeditor/src/test/java/com/b2formeditor/B2formeditorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.b2formeditor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class B2formeditorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
