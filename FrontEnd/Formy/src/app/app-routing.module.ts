cb32b9a241578a8672ff91bf70df3157357604b7
StefanCosminR <stefancosmin.romanescu@gmail.com>
2017-04-29 10:02:39 +0300

    Resolved global dependencies import for access in all components

--- FrontEnd/Formy/src/app/app-routing.module.ts
+++ FrontEnd/Formy/src/app/app-routing.module.ts
@@ -1,15 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialModule } from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+
+import { BuildFormComponent } from "./components/build-form/build-form.component";
 
 const routes: Routes = [
   {
     path: '',
     children: []
+  },
+  {
+    path: 'build-form',
+    component: BuildFormComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    FormsModule,
+    BrowserModule
+  ],
+  exports: [
+    RouterModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    FormsModule,
+    BrowserModule
+  ],
+  declarations: [
+    BuildFormComponent
+  ]
 })
 export class AppRoutingModule { }
--- FrontEnd/Formy/src/app/app.module.ts
+++ FrontEnd/Formy/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import 'hammerjs';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from "@angular/material";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgModule} from "@angular/core";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthentificationComponent } from './components/authentification/authentification.component';
 import { UserFormsComponent } from './components/user-forms/user-forms.component';
-import { BuildFormComponent } from './components/build-form/build-form.component';
 import { FillFormComponent } from './components/fill-form/fill-form.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
 
 
 @NgModule({
@@ -22,18 +16,11 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
     AppComponent,
     AuthentificationComponent,
     UserFormsComponent,
-    BuildFormComponent,
     FillFormComponent,
     NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule,
-    MaterialModule,
-    FlexLayoutModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
