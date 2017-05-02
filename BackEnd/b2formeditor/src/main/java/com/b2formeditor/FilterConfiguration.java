b2742a28c990531de133662c292d2cf680f67cb3
CatalinPJ <CatalinPJ@users.noreply.github.com>
2017-05-03 00:28:51 +0300

    Added filter configuration
    Added filter configuration

---
+++ BackEnd/b2formeditor/src/main/java/com/b2formeditor/FilterConfiguration.java
@@ -0,0 +1,26 @@
+package com.b2formeditor;
+
+import com.b2formeditor.filters.LoginFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Dorneanu Dragos-Andrei on 02.05.2017.
+ */
+@Configuration
+public class FilterConfiguration {
+    @Bean
+    public FilterRegistrationBean loginFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        LoginFilter filter = new LoginFilter();
+
+        registrationBean.setName("LoginFilter");
+        registrationBean.setFilter(filter);
+        registrationBean.setOrder(1);
+        registrationBean.setEnabled(true);
+        registrationBean.setAsyncSupported(true);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+}
