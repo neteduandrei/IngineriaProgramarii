8363a9f80b4b608f14b49ae7d9038882e52ae042
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2017-05-06 21:28:42 +0300

    Fix Login. Added constructor without parameter

--- BackEnd/b2formeditor/src/main/java/com/b2formeditor/models/authenticationmodels/LoginCredentials.java
+++ BackEnd/b2formeditor/src/main/java/com/b2formeditor/models/authenticationmodels/LoginCredentials.java
@@ -4,6 +4,9 @@ package com.b2formeditor.models.authenticationmodels;
  * Created by Dorneanu Dragos-Andrei on 30.04.2017.
  */
 public class LoginCredentials extends AuthenticationCredentials {
+
+    public LoginCredentials() { }
+
     public LoginCredentials(String email, String password) {
         this.email = email;
         this.password = password;
