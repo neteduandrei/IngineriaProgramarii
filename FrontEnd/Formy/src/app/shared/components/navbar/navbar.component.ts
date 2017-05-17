61f5bccf8270ab4accd4ef2d605c37f1397bd8ed
StefanCosminR <stefancosmin.romanescu@gmail.com>
2017-05-17 10:11:41 +0300

    Some icons and design changes

--- FrontEnd/Formy/src/app/shared/components/navbar/navbar.component.ts
+++ FrontEnd/Formy/src/app/shared/components/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
 import { FileSelectDirective, FileDropDirective, FileUploader } from 'ng2-file-upload/ng2-file-upload';
 import {baseUrl} from '../../globals';
+import {MdIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'formy-navbar',
@@ -13,7 +15,10 @@ export class NavbarComponent implements OnInit {
   public uploader:FileUploader = new FileUploader({url: `${baseUrl}/v1/forms`, method: 'PUT', disableMultipart : true});
   public username : string;
 
-  constructor(private authService : AuthService) {
+  constructor(private authService : AuthService, mdIconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
+    mdIconRegistry
+      .addSvgIcon('upload',
+        sanitizer.bypassSecurityTrustResourceUrl('../assets/images/Downloaded.svg'));
   }
 
   public logout() {
